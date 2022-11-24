2a13ea71e85ab657ab63bc76435e369036c05c59
zelal <zelaltunc3449@gmail.com>
2022-11-24 15:14:54 +0300

    deneme

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class app extends StatelessWidget {
+  const app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar: 
+    );
+  }
+}
